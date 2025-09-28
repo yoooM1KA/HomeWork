f3d3e69cb53b3d5810dcb13a44aa23e72d32953e
yoooM1KA <miqayelsanosyan@gmail.com>
2025-09-28 01:41:49 -0700

    obj.js

---
+++ obj.js
@@ -0,0 +1,47 @@
+
+// let arr = [];
+// arr.push(12);
+// arr.unshift(1,2);
+// arr.pop();
+// arr.shift();
+// console.log(arr);
+
+// const numbers = [3,7,10,15,20,25];
+// const ten = numbers.find(num => num >10);
+// console.log(ten);
+// const five = numbers.find(num=> num % 5 === 0);
+// console.log(five);
+
+// const numbers = [5,12,8,130,44];
+// const ten = numbers.filter(num => num > 10);
+// console.log(ten);
+// const even = numbers.filter(num=> num % 2 === 0);
+// console.log(even);
+
+// const words = ["spray", "limit", "elite", "exuberant", "destruction", "present"];
+
+// const long = words.filter(word => word.length > 6 );
+// console.log(long);
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// const double = numbers.map(num => num * 2 );
+// console.log(double);
+// const ten = numbers.map(num => num + 10);
+// console.log(ten);
+
+// const people = [
+//   { name: "Anna", age: 17 },
+//   { name: "Karen", age: 22 },
+//   { name: "Aram", age: 15 },
+//   { name: "Mery", age: 19 }
+// ];
+
+// const arajin = people.find(person => person.age >= 18);
+// console.log(arajin);
+
+// const bidza = people.filter(person => person .age >= 18);
+// console.log(bidza);
+
+// const names = people.map(person => person.name);
+// console.log(names);
