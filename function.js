f68989269681c388f6b6aa7c904d1692b50c0b63
yoooM1KA <miqayelsanosyan@gmail.com>
2025-09-28 10:56:18 -0700

    function.js

---
+++ function.js
@@ -0,0 +1,55 @@
+// let number = prompt('Enter a number : ');
+// let a = number;
+// if (a>0){
+// console.log("1");
+// }
+// else if (a<0){
+// console.log("-1");
+// }
+// else if (a==0){
+// console.log("0");
+// }
+// else{
+// console.log(("You have entered wrong !!!"))
+// }
+
+// let numbers = prompt('Enter 2 numbers to add each other and compare to a hidden number,Good Luck! : ');
+// let sum = numbers[1] + numbers[2];
+// let result = sum < 4 ? true : false;
+// alert(result);
+
+// for (let i = 10;i<=40;i+=2){
+// console.log(i);
+// }
+
+// let sum = 0;
+// for(let i=1;i<=14;i++){
+// sum+=i;
+// }
+// console.log(sum);
+
+// let sum = 0;
+// for(let i = 1;i<=40;i++){
+// if(i%2===0){
+// sum+=i;
+// }
+// }
+// console.log(sum);
+
+// let sum = 0;
+// let i = 1;
+// while (i <=40) {
+// if(i%2!==0){
+// sum+=1;
+// }
+// i++;
+// }
+// console.log(sum);
+
+// let a = 1;
+// for(let i = 1;i<=60;i++){
+// if(i%6===0){
+// a*=i;
+// }
+// }
+// console.log(a);
