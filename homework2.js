90486c11f8c9082b222d04bb9edf2e0bb673b555
yoooM1KA <miqayelsanosyan@gmail.com>
2025-09-28 01:22:37 -0700

    homework2.js

---
+++ homework2.js
@@ -0,0 +1,34 @@
+// let person = {
+//     name: 'Mika',
+//     age: 18,
+//     city: 'Yerevan',
+// }
+// person.isStudent = true;
+// delete person.city;
+
+// if ("age" in person){
+//     alert("Yes the person has an age!!!");
+// }
+// else{
+//     alert("No age!!!");
+// }
+
+// alert(person);
+// let object = {
+//     key1: 'hello',
+//     key2: undefined,
+//     key3: 123,
+//     ket4:undefined
+// }
+// function remove(a){
+//     for(let key in object){
+//         if(object[key]===undefined){
+//             delete object[key];
+//         }
+//     }
+//     return object;
+// }
+
+// remove(object);
+// console.log(object);
+
